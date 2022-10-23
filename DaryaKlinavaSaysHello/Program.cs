1e3c304ded393b22d16c2150202abb39c1500f42
Darya Klinava <darya.klinava@itechart-group.com>
2022-10-23 23:05:46 +0200

    Add hometask1

---
+++ DaryaKlinavaSaysHello/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hi! What’s your name?");
+var name = Console.ReadLine();
+Console.WriteLine($"Nice to meet you {name}");
