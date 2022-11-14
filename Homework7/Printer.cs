15d8d1ab458179624d8fb23358a4d6e298173f65
Darya Klinava <darya.klinava@itechart-group.com>
2022-11-14 12:10:02 +0100

    task 7.3

---
+++ Homework7/Printer.cs
@@ -0,0 +1,40 @@
+﻿using Homework7;
+
+namespace AutomationCources.Lecture_7.Homework
+{
+    public class Printer : Technic, IPrinting
+    {
+        private int paperWidth;
+        private int paperHeight;
+
+        public Printer(string? modelName, decimal price, int paperWidth, int paperHeight)
+            :base(modelName, price)
+        {
+            this.paperWidth = paperWidth;
+            this.paperHeight = paperHeight;
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return $"Price: {Price}, model:{ModelName}";
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Printing...");
+        }
+
+        public override void TurnOn()
+        {
+            Console.WriteLine("Press button at the top");
+        }
+
+        public override void TurnOff()
+        {
+            Console.WriteLine("Press Turn Off button");
+        }
+    }
+}
